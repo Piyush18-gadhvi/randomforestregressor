d4a91ad825fe4643e647136850db455f13713d1a
Piyushdan Ambadan Mod <52657083+Piyush18-gadhvi@users.noreply.github.com>
2021-10-22 21:24:38 +0530

    Update train.py

--- train.py
+++ train.py
@@ -3,15 +3,15 @@ import pandas as pd
 import mlflow
 
 dataset = pd.read_csv('https://raw.githubusercontent.com/mk-gurucharan/Regression/master/IceCreamData.csv')
-X = dataset['Temperature'].values
-y = dataset['Revenue'].values
+X = pd.read_csv("X_train.csv")
+y = pd.read_csv("y_train.csv")
 
-from sklearn.model_selection import train_test_split
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.3)
+#from sklearn.model_selection import train_test_split
+#X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.3)
 
 # Fitting Random Forest Regression to the dataset
 from sklearn.ensemble import RandomForestRegressor
 with mlflow.start_run():
         regressor = RandomForestRegressor(n_estimators = 10, random_state = 0)
-        regressor.fit(X_train(-1,1), y_train.reshape(-1,1))
+        regressor.fit(X(-1,1), y.reshape(-1,1))
         mlflow.sklearn.log_model(regressor, "model")
