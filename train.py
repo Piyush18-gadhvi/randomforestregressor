bd6dd1fd3f3e85024ed29cf34d2e3d11bfd6180a
Piyushdan Ambadan Mod <52657083+Piyush18-gadhvi@users.noreply.github.com>
2021-10-22 21:19:41 +0530

    Update train.py

--- train.py
+++ train.py
@@ -7,7 +7,7 @@ X = dataset['Temperature'].values
 y = dataset['Revenue'].values
 
 from sklearn.model_selection import train_test_split
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.3, random_state = 42)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.3)
 
 # Fitting Random Forest Regression to the dataset
 from sklearn.ensemble import RandomForestRegressor
