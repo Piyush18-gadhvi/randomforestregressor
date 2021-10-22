5e3c1132514f43a5cd4af0f33804485855085168
Piyushdan Ambadan Mod <52657083+Piyush18-gadhvi@users.noreply.github.com>
2021-10-22 20:40:57 +0530

    Update train.py

--- train.py
+++ train.py
@@ -7,7 +7,7 @@ X = dataset['Temperature'].values
 y = dataset['Revenue'].values
 
 from sklearn.model_selection import train_test_split
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.3)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.3, random_state=42)
 
 # Fitting Random Forest Regression to the dataset
 from sklearn.ensemble import RandomForestRegressor
@@ -15,6 +15,6 @@ from sklearn.ensemble import RandomForestRegressor
 
 
 with mlflow.start_run():
-        regressor = RandomForestRegressor(n_estimators = 10, random_state = 0)
-        regressor.fit(X_train.reshape(-1,1), y_train.reshape(-1,1))
+        regressor = RandomForestRegressor(n_estimators = 10, random_state = 42)
+        regressor.fit(X_train(-1,1), y_train.reshape(-1,1))
         mlflow.sklearn.log_model(regressor, "model")
