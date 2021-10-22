819ee65f55ec91af368e714d7033b4db26691e25
Piyushdan Ambadan Mod <52657083+Piyush18-gadhvi@users.noreply.github.com>
2021-10-22 20:52:58 +0530

    Update train.py

--- train.py
+++ train.py
@@ -11,7 +11,6 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.3, rando
 
 # Fitting Random Forest Regression to the dataset
 from sklearn.ensemble import RandomForestRegressor
-
 with mlflow.start_run():
         regressor = RandomForestRegressor(n_estimators = 10, random_state = 0)
         regressor.fit(X_train(-1,1), y_train.reshape(-1,1))
