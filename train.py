8312415d3a472bf28580f8c0ddb2e17413f4b7ed
Piyush18-gadhvi <gadhvipiyush705@gmail.com>
2021-10-22 18:15:16 +0530

    random forest regression

---
+++ train.py
@@ -0,0 +1,20 @@
+import numpy as np
+import pandas as pd
+import mlflow
+
+dataset = pd.read_csv('https://raw.githubusercontent.com/mk-gurucharan/Regression/master/IceCreamData.csv')
+X = dataset['Temperature'].values
+y = dataset['Revenue'].values
+
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.3)
+
+# Fitting Random Forest Regression to the dataset
+from sklearn.ensemble import RandomForestRegressor
+
+
+
+with mlflow.start_run():
+        regressor = RandomForestRegressor(n_estimators = 10, random_state = 0)
+        regressor.fit(X_train.reshape(-1,1), y_train.reshape(-1,1))
+        mlflow.sklearn.log_model(lr, "model")
