a6f02969ea0824a57d3702b2e69a879da2b6cb15
Piyushdan Ambadan Mod <52657083+Piyush18-gadhvi@users.noreply.github.com>
2021-10-25 12:38:05 +0530

    Update train.py

--- train.py
+++ train.py
@@ -10,8 +10,12 @@ y = pd.read_csv("y_train.csv")
 #X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.3)
 
 # Fitting Random Forest Regression to the dataset
+n_estimators_ = int(sys.argv[1]) #if len(sys.argv) > 1 else 0.5
+random_state_ = int(sys.argv[2]) #if len(sys.argv) > 2 else 0.5
 from sklearn.ensemble import RandomForestRegressor
 with mlflow.start_run():
-        regressor = RandomForestRegressor(n_estimators = 10, random_state = 0)
+        regressor = RandomForestRegressor(n_estimators = n_estimators_, random_state = random_state_)
         regressor.fit(X, y)
+        mlflow.log_param("alpha", alpha)
+        mlflow.log_param("l1_ratio", l1_ratio)
         mlflow.sklearn.log_model(regressor, "model")
